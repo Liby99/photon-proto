97d9e6334360e5f2f8720536ad92ca1922e5f312
Liby Lee <liby99@icloud.com>
2019-07-28 12:27:43 -0400

    Done speed test

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,13 @@
 #[macro_use]
 extern crate neon;
 
-use std::time::{Duration, Instant};
 use neon::prelude::*;
 
-fn fill_black(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+fn render(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+  Ok(cx.undefined())
+}
 
-  let begin = Instant::now();
+fn fill_black(mut cx: FunctionContext) -> JsResult<JsUndefined> {
 
   let img_data: Handle<JsObject> = cx.argument::<JsObject>(0)?;
   let width = img_data.get(&mut cx, "width")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as usize;
@@ -14,13 +15,10 @@ fn fill_black(mut cx: FunctionContext) -> JsResult<JsUndefined> {
   let mut buffer = img_data.get(&mut cx, "data")?.downcast::<JsBuffer>().unwrap_or(cx.buffer(0)?);
   let buffer_length = buffer.get(&mut cx, "length")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as usize;
 
-  let fetch = Instant::now();
-  println!("Fetch data elapsed: {:?}", fetch.duration_since(begin));
-
-  {
+  { // Tricks to get rid of borrow checker
     let guard = cx.lock();
     let data = buffer.borrow_mut(&guard);
-    let slice: &[u8] = data.as_mut_slice::<u8>();
+    let slice = data.as_mut_slice::<u8>();
 
     for x in 0..width {
       for y in 0..height {
@@ -35,11 +33,11 @@ fn fill_black(mut cx: FunctionContext) -> JsResult<JsUndefined> {
     }
   }
 
-  println!("Fill pixels elapsed: {:?}", Instant::now().duration_since(fetch));
   Ok(cx.undefined())
 }
 
 register_module!(mut cx, {
+  cx.export_function("render", render)?;
   cx.export_function("fillBlack", fill_black)?;
   Ok(())
 });
