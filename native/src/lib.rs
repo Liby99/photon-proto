a3e50a2fc64809d52b491118004fc4c125c0a2d0
Liby Lee <liby99@icloud.com>
2019-07-28 12:23:30 -0400

    Done raw buffer filling which is super fast!

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -9,26 +9,28 @@ fn fill_black(mut cx: FunctionContext) -> JsResult<JsUndefined> {
   let begin = Instant::now();
 
   let img_data: Handle<JsObject> = cx.argument::<JsObject>(0)?;
-  let width = img_data.get(&mut cx, "width")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as u32;
-  let height = img_data.get(&mut cx, "height")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as u32;
-  let buffer = img_data.get(&mut cx, "data")?.downcast::<JsBuffer>().unwrap_or(cx.buffer(0)?);
-  let buffer_length = buffer.get(&mut cx, "length")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as u32;
+  let width = img_data.get(&mut cx, "width")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as usize;
+  let height = img_data.get(&mut cx, "height")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as usize;
+  let mut buffer = img_data.get(&mut cx, "data")?.downcast::<JsBuffer>().unwrap_or(cx.buffer(0)?);
+  let buffer_length = buffer.get(&mut cx, "length")?.downcast::<JsNumber>().unwrap_or(cx.number(0)).value() as usize;
 
   let fetch = Instant::now();
   println!("Fetch data elapsed: {:?}", fetch.duration_since(begin));
 
-  for x in 0..width {
-    for y in 0..height {
-      let index = (y * width + x) * 4;
-      if index + 3 < buffer_length {
-        let r = cx.number(0);
-        let g = cx.number(0);
-        let b = cx.number(0);
-        let a = cx.number(255);
-        buffer.set(&mut cx, index, r)?;
-        buffer.set(&mut cx, index + 1, g)?;
-        buffer.set(&mut cx, index + 2, b)?;
-        buffer.set(&mut cx, index + 3, a)?;
+  {
+    let guard = cx.lock();
+    let data = buffer.borrow_mut(&guard);
+    let slice: &[u8] = data.as_mut_slice::<u8>();
+
+    for x in 0..width {
+      for y in 0..height {
+        let index: usize = (y * width + x) * 4;
+        if index + 3 < buffer_length {
+          slice[index] = 0;
+          slice[index + 1] = 0;
+          slice[index + 2] = 0;
+          slice[index + 3] = 255;
+        }
       }
     }
   }
