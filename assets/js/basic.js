053d1a573cb6165c463f86aa7a214576ada3cf85
Liby Lee <liby99@icloud.com>
2019-07-27 23:07:13 -0400

    Able to use js to render black squares

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,1 +1,11 @@
-alert("Hello World");
+const Photon = require("../../lib/index");
+
+const canvas = document.getElementById("main-canvas");
+const { width, height } = canvas;
+const context = canvas.getContext("2d");
+const imgData = context.createImageData(width, height);
+const imgBuffer = imgData.data;
+
+Photon.render({}, imgBuffer);
+
+context.putImageData(imgData, 0, 0);
