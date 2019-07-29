0afd29be6aafdba5e255dc56eaa26fb8cd6f7fb5
Liby Lee <liby99@icloud.com>
2019-07-29 00:21:45 -0400

    Compilable more stuff

--- native/src/intersectable.rs
+++ native/src/intersectable.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub, Mul, Div};
+use std::ops::{Add, Sub, Mul, Div, Neg};
 
 #[derive(Clone)]
 pub struct Vector3 {
@@ -8,16 +8,60 @@ pub struct Vector3 {
 }
 
 impl Vector3 {
+  pub fn zero() -> Vector3 {
+    Vector3 { x: 0.0, y: 0.0, z: 0.0 }
+  }
+
+  pub fn new(x: f32, y: f32, z: f32) -> Vector3 {
+    Vector3 { x, y, z }
+  }
+
+  pub fn i() -> Vector3 {
+    Vector3 { x: 1.0, y: 0.0, z: 0.0 }
+  }
+
+  pub fn j() -> Vector3 {
+    Vector3 { x: 0.0, y: 1.0, z: 0.0 }
+  }
+
+  pub fn k() -> Vector3 {
+    Vector3 { x: 0.0, y: 0.0, z: 1.0 }
+  }
+
   pub fn dot(&self, other: &Vector3) -> f32 {
     self.x * other.x + self.y * other.y + self.z * other.z
   }
+
+  pub fn max(&self, other: &Vector3) -> Vector3 {
+    Vector3 {
+      x: self.x.max(other.x),
+      y: self.y.max(other.y),
+      z: self.z.max(other.z),
+    }
+  }
+
+  pub fn min(&self, other: &Vector3) -> Vector3 {
+    Vector3 {
+      x: self.x.min(other.x),
+      y: self.y.min(other.y),
+      z: self.z.min(other.z),
+    }
+  }
 }
 
-impl Add<&Vector3> for &Vector3 {
-  type Output = Vector3;
+impl Neg for Vector3 {
+  type Output = Self;
 
-  fn add(self, rhs: &Vector3) -> Vector3 {
-    Vector3 {
+  fn neg(self) -> Self {
+    Vector3 { x: -self.x, y: -self.y, z: -self.z }
+  }
+}
+
+impl Add<Vector3> for Vector3 {
+  type Output = Self;
+
+  fn add(self, rhs: Self) -> Self {
+    Self {
       x: self.x + rhs.x,
       y: self.y + rhs.y,
       z: self.z + rhs.z,
@@ -25,11 +69,11 @@ impl Add<&Vector3> for &Vector3 {
   }
 }
 
-impl Sub<&Vector3> for &Vector3 {
-  type Output = Vector3;
+impl Sub<Vector3> for Vector3 {
+  type Output = Self;
 
-  fn sub(self, rhs: &Vector3) -> Vector3 {
-    Vector3 {
+  fn sub(self, rhs: Self) -> Self {
+    Self {
       x: self.x - rhs.x,
       y: self.y - rhs.y,
       z: self.z - rhs.z,
@@ -37,11 +81,11 @@ impl Sub<&Vector3> for &Vector3 {
   }
 }
 
-impl Mul<f32> for &Vector3 {
-  type Output = Vector3;
+impl Mul<f32> for Vector3 {
+  type Output = Self;
 
-  fn mul(self, rhs: f32) -> Vector3 {
-    Vector3 {
+  fn mul(self, rhs: f32) -> Self {
+    Self {
       x: self.x * rhs,
       y: self.y * rhs,
       z: self.z * rhs,
@@ -49,14 +93,26 @@ impl Mul<f32> for &Vector3 {
   }
 }
 
-impl Div<f32> for &Vector3 {
-  type Output = Vector3;
+impl Div<f32> for Vector3 {
+  type Output = Self;
 
-  fn div(self, rhs: f32) -> Vector3 {
-    Vector3 {
+  fn div(self, rhs: f32) -> Self {
+    Self {
       x: self.x / rhs,
       y: self.y / rhs,
       z: self.z / rhs,
+    }
+  }
+}
+
+impl Div<Vector3> for Vector3 {
+  type Output = Self;
+
+  fn div(self, rhs: Vector3) -> Self {
+    Self {
+      x: self.x / rhs.x,
+      y: self.y / rhs.y,
+      z: self.z / rhs.z,
     }
   }
 }
@@ -68,7 +124,7 @@ pub struct Ray {
 
 impl Ray {
   pub fn point_at(&self, t: f32) -> Vector3 {
-    &self.origin + &(&self.direction * t)
+    self.origin.clone() + self.direction.clone() * t
   }
 }
 
@@ -94,11 +150,41 @@ pub struct Sphere {
 
 pub struct Plane;
 
-// impl Intersectable for Cube {
-//   fn intersect(self, ray: &Ray) -> Option<IntersectionInfo> {
-
-//   }
-// }
+impl Intersectable for Cube {
+  fn intersect(self, ray: &Ray) -> Option<IntersectionInfo> {
+    let hx = self.size_x / 2.0;
+    let hy = self.size_y / 2.0;
+    let hz = self.size_z / 2.0;
+    let min_corner = Vector3 { x: -hx, y: -hy, z: -hz };
+    let max_corner = Vector3 { x: hx, y: hy, z: hz };
+    let t_min_tmp = (min_corner - ray.origin.clone()) / ray.direction.clone();
+    let t_max_tmp = (max_corner - ray.origin.clone()) / ray.direction.clone();
+    let t_min_vec = t_min_tmp.min(&t_max_tmp);
+    let t_max_vec = t_min_tmp.max(&t_max_tmp);
+    let t_min = t_min_vec.x.max(t_min_vec.y).max(t_min_vec.z);
+    let t_max = t_max_vec.x.min(t_max_vec.y).min(t_max_vec.z);
+    if t_max - t_min < 0.0 {
+      return None;
+    } else {
+      let (t, sign) = if t_min > 0.0 && t_max > 0.0 {
+        (t_min, 1.0)
+      } else if t_min < 0.0 && t_max > 0.0 {
+        (t_max, -1.0)
+      } else {
+        return None;
+      };
+      let position = ray.point_at(t);
+      let normal = if t == t_min_vec.y || t == t_max_vec.y {
+        if position.y > 0.0 { Vector3::j() } else { -Vector3::j() }
+      } else if t == t_min_vec.x || t == t_min_vec.x {
+        if position.x > 0.0 { Vector3::i() } else { -Vector3::i() }
+      } else {
+        if position.z > 0.0 { Vector3::k() } else { -Vector3::k() }
+      } * sign;
+      Some(IntersectionInfo { position, normal, t })
+    }
+  }
+}
 
 impl Intersectable for Sphere {
   fn intersect(self, ray: &Ray) -> Option<IntersectionInfo> {
